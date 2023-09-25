f658d2d9a5eb551cc7f850d46d7c38bc7728798d
Alejandro Eyquem <fabriziorossi@vivaldi.net>
2023-09-25 17:01:28 -0300

    Created reusable modal

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <Teleport to="#modal">
+    <dialog v-if="visible" class="modal__container">
+      <slot> </slot>
+    </dialog>
+  </Teleport>
+</template>
+<script setup>
+defineProps({
+  visible: Boolean,
+});
+</script>
+<style>
+.modal__container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
