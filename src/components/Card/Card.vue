85fa5490325f88065d28d371016fa24690ce1946
Alejandro Eyquem <fabriziorossi@vivaldi.net>
2023-09-26 02:39:08 -0300

    Added flip card animation

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,13 +1,18 @@
 <template>
-  <figure class="w-24 h-36 border-[1px] border-white-200 rounded-lg">
-    <img
-      v-if="isFlipped"
-      :src="card.src"
-      :alt="card.slug"
-      :title="card.slug"
-      class="w-full h-full object-cover rounded-lg"
-    />
-    <img v-else class="bg-[url('@/assets/card.webp')] w-full h-full rounded-lg" />
+  <figure class="w-24 h-36 rounded-lg">
+    <Transition name="flip" mode="out-in">
+      <img
+        v-if="isFlipped"
+        :src="card.src"
+        :alt="card.slug"
+        :title="card.slug"
+        class="w-full h-full object-cover rounded-lg border-[1px] border-amber-300"
+      />
+      <img
+        v-else
+        class="bg-[url('@/assets/card.webp')] w-full h-full rounded-lg border-[1px] border-white-200"
+      />
+    </Transition>
   </figure>
 </template>
 
@@ -20,3 +25,14 @@ defineProps({
   isFlipped: Boolean,
 });
 </script>
+<style scoped>
+.flip-enter-from,
+.flip-leave-to {
+  transform: rotateY(90deg);
+}
+
+.flip-enter-active,
+.flip-leave-active {
+  transition: all 0.1s ease;
+}
+</style>
