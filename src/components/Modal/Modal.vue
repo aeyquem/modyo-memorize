e1734875b708e88924153431b44333425d935e03
Alejandro Eyquem <fabriziorossi@vivaldi.net>
2023-09-26 01:37:32 -0300

    Moved modal styles to tailwindcss

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,6 +1,9 @@
 <template>
   <Teleport to="#modal">
-    <dialog v-if="visible" class="modal__container">
+    <dialog
+      v-if="visible"
+      class="fixed inset-0 flex justify-center items-center w-full h-full bg-[rgba(209,209,209,0.23)] p-5 bg-opacity-60 backdrop-filter backdrop-blur-sm"
+    >
       <slot> </slot>
     </dialog>
   </Teleport>
@@ -10,23 +13,3 @@ defineProps({
   visible: Boolean,
 });
 </script>
-<style>
-.modal__container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-
-  background-color: rgba(0, 0, 0, 0.5);
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  backdrop-filter: blur(8px) saturate(103%);
-  -webkit-backdrop-filter: blur(8px) saturate(103%);
-  background-color: rgba(209, 209, 209, 0.66);
-  border: 1px solid rgba(209, 213, 219, 0.3);
-}
-</style>
